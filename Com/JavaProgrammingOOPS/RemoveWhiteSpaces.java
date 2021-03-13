95c9af386949267c5ca7be9067eb3c70375ca405
Sagar Ingale <sagar.i@continuousvalidation.com>
2021-03-13 20:39:08 +0530

    commit code

---
+++ Com/JavaProgrammingOOPS/RemoveWhiteSpaces.java
@@ -0,0 +1,15 @@
+package Com.JavaProgrammingOOPS;
+
+//Write a java program to all white spaces in String
+
+public class RemoveWhiteSpaces {
+
+	public static void main(String[] args) {
+		
+		String str = " j a va s ta r ";
+		String str1 = str.replaceAll("\\s", "");
+		System.out.println(str1);
+		
+	}
+
+}
